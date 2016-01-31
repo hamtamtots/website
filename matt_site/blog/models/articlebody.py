343b1ef18992205430879688d5a2cd1adaf942de
Matt Thomas <i-am-matt@hotmail.co.uk>
2016-01-31 15:52:08 +0000

    added support for images in articles using static template tags

--- matt_site/blog/models/articlebody.py
+++ matt_site/blog/models/articlebody.py
@@ -4,3 +4,8 @@ class ArticleBody(models.Model):
 
     id = models.AutoField(primary_key = True)
     body = models.TextField()
+
+    readonly_fields = ('id',)
+
+    def __str__(self):
+        return "{0}".format(self.id)
--- matt_site/blog/views/articleview.py
+++ matt_site/blog/views/articleview.py
@@ -1,15 +1,15 @@
 ﻿from django.views.generic import DetailView
 from django.shortcuts import get_object_or_404
+from django.template import Template
 from markdown import markdown
 
 from blog.models.article import Article
 
 class ArticleView(DetailView):
     
-    model = Article
     template_name = 'blog/article.html'
 
     def get_object(self):
         article = get_object_or_404(Article.objects.select_related('body').prefetch_related('tags'), key=self.kwargs['key'])
-        article.body.body = markdown(article.body.body)
+        article.body_template = Template(markdown(article.body.body))
         return article
